23af3ec3877cf985c9d9205d052a3a4c50cdd900
eyal <eyal444555@gmail.com>
2020-09-23 16:28:21 +0300

    Updates

---
+++ precache-manifest.df76c95d3932be8dbf705dbc9b21a9be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "487ac9323794ce37d5c5f93410240e3f",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "169cabcafa68c3625a26",
+    "url": "/pokedex/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4cfccf4b331799f5b564",
+    "url": "/pokedex/static/css/main.b45f2963.chunk.css"
+  },
+  {
+    "revision": "169cabcafa68c3625a26",
+    "url": "/pokedex/static/js/2.a2dad045.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokedex/static/js/2.a2dad045.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cfccf4b331799f5b564",
+    "url": "/pokedex/static/js/main.156bbe61.chunk.js"
+  },
+  {
+    "revision": "dd6bddbc3b7842f642db",
+    "url": "/pokedex/static/js/runtime-main.ed7addec.js"
+  },
+  {
+    "revision": "7216aef7fd7788605af8e218cf4d0dfe",
+    "url": "/pokedex/static/media/Spinner.7216aef7.gif"
+  }
+]);
